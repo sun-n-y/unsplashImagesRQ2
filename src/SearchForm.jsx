964e35c758cb605d1b59d8393ca65a3f111a5cdf
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-07 12:12:59 -0500

    add form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,26 @@
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements[0].value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit" className="btn ">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
