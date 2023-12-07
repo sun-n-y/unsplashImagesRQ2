4058b77d4cf40a1b1f6614034d216d5d2f0147c5
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-07 12:59:42 -0500

    add gallery search

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const key = import.meta.env.VITE_ACCESS_KEY;
+
+const url = `https://api.unsplash.com/search/photos/?client_id=${key}&query=cat`;
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { data } = useQuery({
+    queryKey: ['photos'],
+    queryFn: () => axios.get(url),
+  });
+
+  if (!data) {
+    return <div className="loading">loading...</div>;
+  }
+
+  return (
+    <div className="image-container">
+      {data.data.results.map((item) => {
+        const { urls, alt_description: desc, id } = item;
+        return <img key={id} src={urls.regular} alt={desc} className="img" />;
+      })}
+    </div>
+  );
 };
 export default Gallery;
