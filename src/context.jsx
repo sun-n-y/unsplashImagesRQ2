91cee8aec7536a814fa5b88aec3416dee030b76b
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-08 11:55:47 -0500

    add darkmode enchanced

--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,36 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme');
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode;
+  }
+
+  return storedDarkMode === 'true';
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState('tiger');
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
     const body = document.querySelector('body');
     body.classList.toggle('dark-theme', newDarkTheme);
   };
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', isDarkTheme);
+  }, []);
 
   return (
     <AppContext.Provider
