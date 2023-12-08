1f9128844ae3c6e2f7cb5f807f8b6ccfbe895171
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-08 11:14:22 -0500

    add render images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -6,24 +6,43 @@ const key = import.meta.env.VITE_ACCESS_KEY;
 const url = `https://api.unsplash.com/search/photos/?client_id=${key}&query=cat`;
 
 const Gallery = () => {
-  const { data } = useQuery({
+  const response = useQuery({
     queryKey: ['photos'],
     queryFn: async () => {
-      const response = await axios.get(url);
-      return response;
+      const result = await axios.get(url);
+      return result;
     },
   });
-  console.log(data);
 
-  if (!data) {
-    return <div className="loading">loading...</div>;
+  if (response.isLoading) {
+    return (
+      <div className="image-container">
+        <div className="loading"></div>
+      </div>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <div className="image-container">
+        <h4>there was an error...</h4>;
+      </div>
+    );
+  }
+
+  if (response.data.data.results.length < 1) {
+    return (
+      <div className="image-container">
+        <h4>no results found...</h4>
+      </div>
+    );
   }
 
   return (
     <div className="image-container">
-      {data.data.results.map((item) => {
+      {response.data.data.results.map((item) => {
         const { urls, alt_description: desc, id } = item;
-        return <img key={id} src={urls.regular} alt={desc} className="img" />;
+        return <img key={id} src={urls?.regular} alt={desc} className="img" />;
       })}
     </div>
   );
